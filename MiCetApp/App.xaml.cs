0c593a6112a23fffb2a482f4a0970bec7e28628c
syachrodi <syachrodi@gmail.com>
2023-01-29 17:05:07 +0700

    tes

--- MiCetApp/App.xaml.cs
+++ MiCetApp/App.xaml.cs
@@ -8,7 +8,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new NavigationPage(new MainPage());
+		MainPage = new NavigationPage(new StartPage());
 		/*MainPage = new AppShell();*/
     }
 }
--- MiCetApp/View/StartPage.xaml.cs
+++ MiCetApp/View/StartPage.xaml.cs
@@ -8,4 +8,14 @@ public partial class StartPage : ContentPage
         NavigationPage.SetHasNavigationBar(this, false);
         NavigationPage.SetHasBackButton(this, false);
     }
+
+    private void PlayAnimation(object sender, EventArgs e)
+    {
+        gifAnimation.IsAnimationPlaying = true;
+    }
+
+    private void PauseAnimation(object sender, EventArgs e)
+    {
+        gifAnimation.IsAnimationPlaying = false;
+    }
 }
